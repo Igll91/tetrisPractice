d282ddcddf7bcd7418d3a89a097861fd0546a44b
Silvio <bfsilviovuk@gmail.com>
2013-12-09 19:07:15 +0100

    malo sređena rotacija i odnos između shape klase i podklasa.
    dakle podklase samo definiraju veličinu i izgled, sve ostalo sređiva shape.

--- src/silvio/vuk/gamedev/tetrisboxes/entitys/IShape.java
+++ src/silvio/vuk/gamedev/tetrisboxes/entitys/IShape.java
@@ -2,23 +2,23 @@ package silvio.vuk.gamedev.tetrisboxes.entitys;
 
 import javax.management.InvalidAttributeValueException;
 
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 
 public class IShape extends Shape {
 	
-	private Rectangle rotationRectangleChecker; // kako provjeriti sa Fieldom dali se preklapa ?... još razmislitit malo o tome...
-	private final int NUMBER_OF_BOXES_IN_SHAPE  = 4;
-	private final int NUMBER_OF_BOXES_IN_ROW 	= 4;
-	private final int NUMBER_OF_BOXES_IN_COLUMN = 4;
-	
-	public IShape()
+	public IShape() throws InvalidAttributeValueException
 	{
+		super(4, 4, 4);
+		
 		super.arrayOfBoxes = new Array<>(NUMBER_OF_BOXES_IN_SHAPE);
 		createRotationShapes();
+		testMapOfRotationStates();
+		
+		// inicijalizacija prvih elemenata u objektu !
 	}
 	
-	private void createRotationShapes()
+	@Override
+	protected void createRotationShapes()
 	{
 		String [] positions = new String[NUMBER_OF_BOXES_IN_SHAPE];
 		
@@ -51,59 +51,5 @@ public class IShape extends Shape {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	private void rotateObject(boolean rotateDirection)
-	{
-		// trenutni rotationShape
-		int lastRotationShapeCenterBlockPositionX = super.centerBox.getX();
-		int lastRotationShapeCenterBlockPositionY = super.centerBox.getX();
 		
-		
-		
-	}
-	
-	private int[] getCurrentRotationStateCenterBlockPosition() throws InvalidAttributeValueException
-	{
-		int [] centerBlockPosition = new int[2];
-		
-		int pos = super.mapOfRotationStates.get(currentRotationState).indexOf("C");
-		
-		if(pos == -1)
-			throw new InvalidAttributeValueException("No center notation found in rotation states! currentRotationState = " +currentRotationState);
-		
-		
-		
-		return centerBlockPosition;
-	}
-	
-	private void testMapOfRotationStates() throws InvalidAttributeValueException
-	{
-		for(int counter = 0; counter < NUMBER_OF_BOXES_IN_SHAPE; counter++ )
-		{
-			String currentRotationState = mapOfRotationStates.get(counter);
-			int numberOfBlocks = 0;
-			int numberOfCenterBlocks = 0;
-			
-			for(int counter_row = 0; counter_row < NUMBER_OF_BOXES_IN_ROW; counter_row++)
-			{
-				for(int counter_column = 0; counter_column < NUMBER_OF_BOXES_IN_COLUMN; counter_column++)
-				{
-					char testChar = currentRotationState.charAt(counter_row * NUMBER_OF_BOXES_IN_ROW + counter_column);
-					
-					switch(testChar)
-					{
-					case 'x':
-						break;
-					case 'B':
-						break;
-					case 'C':
-						break;
-					default:
-						throw new InvalidAttributeValueException("Invalid character in currentRotationState! " +
-								"\ncurrentRotationState key = " + counter);
-					}
-				}
-			}
-		}
-	}
 }
